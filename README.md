5d8906cd78080d8e0e4a9918ccb822942a04a5ed
Pierre <pierredureau@yahoo.fr>
2020-02-09 23:21:51 +0100

    Add composer.json; Add theme discovery

---
+++ README.md
@@ -0,0 +1,7 @@
+Very inspired by https://www.drupal.org/project/layout_builder_styles/
+
+But:
+
+* plugin-based instead of config entity-based, to (see also: )
+* groups (see also: )
+* with a library  (see also: )
--- src/StylePluginManager.php
+++ src/StylePluginManager.php
@@ -5,6 +5,7 @@ namespace Drupal\layout_builder_classes;
 use Drupal\Component\Plugin\Exception\PluginException;
 use Drupal\Core\Cache\CacheBackendInterface;
 use Drupal\Core\Extension\ModuleHandlerInterface;
+use Drupal\Core\Extension\ThemeHandlerInterface;
 use Drupal\Core\Plugin\DefaultPluginManager;
 use Drupal\Core\Plugin\Discovery\ContainerDerivativeDiscoveryDecorator;
 use Drupal\Core\Plugin\Discovery\YamlDiscovery;
@@ -13,6 +14,13 @@ use Drupal\Core\Plugin\Discovery\YamlDiscovery;
  * Provides the default style_plugin manager.
  */
 class StylePluginManager extends DefaultPluginManager implements StylePluginManagerInterface {
+
+  /**
+   * The theme handler.
+   *
+   * @var \Drupal\Core\Extension\ThemeHandlerInterface
+   */
+  protected $themeHandler;
 
   /**
    * Provides default values for all style_plugin plugins.
@@ -31,12 +39,14 @@ class StylePluginManager extends DefaultPluginManager implements StylePluginMana
    *
    * @param \Drupal\Core\Extension\ModuleHandlerInterface $module_handler
    *   The module handler.
+   * @param \Drupal\Core\Extension\ThemeHandlerInterface $theme_handler
+   *   The theme handler.
    * @param \Drupal\Core\Cache\CacheBackendInterface $cache_backend
    *   Cache backend instance to use.
    */
-  public function __construct(ModuleHandlerInterface $module_handler, CacheBackendInterface $cache_backend) {
-    // Add more services as required.
+  public function __construct(ModuleHandlerInterface $module_handler, ThemeHandlerInterface $theme_handler, CacheBackendInterface $cache_backend) {
     $this->moduleHandler = $module_handler;
+    $this->themeHandler = $theme_handler;
     $this->setCacheBackend($cache_backend, 'style_plugin', ['style_plugin']);
   }
 
@@ -45,7 +55,7 @@ class StylePluginManager extends DefaultPluginManager implements StylePluginMana
    */
   protected function getDiscovery() {
     if (!isset($this->discovery)) {
-      $this->discovery = new YamlDiscovery('style.plugin', $this->moduleHandler->getModuleDirectories());
+      $this->discovery = new YamlDiscovery('style.plugin', $this->moduleHandler->getModuleDirectories() + $this->themeHandler->getThemeDirectories());
       $this->discovery->addTranslatableProperty('label', 'label_context');
       $this->discovery = new ContainerDerivativeDiscoveryDecorator($this->discovery);
     }
@@ -61,6 +71,13 @@ class StylePluginManager extends DefaultPluginManager implements StylePluginMana
     if (empty($definition['id'])) {
       throw new PluginException(sprintf('Example plugin property (%s) definition "is" is required.', $plugin_id));
     }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function providerExists($provider) {
+    return $this->moduleHandler->moduleExists($provider) || $this->themeHandler->themeExists($provider);
   }
 
 }
