309120433b4fd468cea4bd6d228e5879d0bee4f6
Florent Torregrosa <14238-florenttorregrosa@users.noreply.drupalcode.org>
2023-11-09 10:51:00 +0000

    Issue #3399611 by Grimreaper: Drupal 10.1: Layout Builder: Styles preview no more working on sections

--- modules/ui_styles_layout_builder/src/LayoutBuilderTrustedCallbacks.php
+++ modules/ui_styles_layout_builder/src/LayoutBuilderTrustedCallbacks.php
@@ -26,22 +26,33 @@ class LayoutBuilderTrustedCallbacks implements TrustedCallbackInterface {
    */
   public static function preRender(array $element): array {
     $styles_manager = \Drupal::service('plugin.manager.ui_styles');
-    $layout_builder = $element['layout_builder'];
     /** @var \Drupal\layout_builder\SectionStorageInterface $section_storage */
     $section_storage = $element['#section_storage'];
     $delta = 0;
     $max = \count($section_storage->getSections());
-    foreach (Element::children($layout_builder) as $index) {
-      if (isset($layout_builder[$index]['layout-builder__section']) && $delta < $max) {
+    foreach (Element::children($element['layout_builder']) as $index) {
+      foreach (Element::children($element['layout_builder'][$index]) as $layout_index) {
+        if ($delta >= $max) {
+          break 2;
+        }
+
+        if (!\is_numeric($layout_index)) {
+          continue;
+        }
+        $layout = &$element['layout_builder'][$index][$layout_index];
+        if (!isset($layout['#layout'])) {
+          continue;
+        }
+
+        // Section styles.
         $section = $section_storage->getSection($delta);
         /** @var array $selected */
         $selected = $section->getThirdPartySetting('ui_styles', 'selected') ?: [];
         /** @var string $extra */
         $extra = $section->getThirdPartySetting('ui_styles', 'extra') ?: '';
-        $element['layout_builder'][$index]['layout-builder__section'] =
-          $styles_manager->addClasses($element['layout_builder'][$index]['layout-builder__section'], $selected, $extra);
+        $layout = $styles_manager->addClasses($layout, $selected, $extra);
 
-        // Regions.
+        // Regions styles.
         /** @var array $regions_configuration */
         $regions_configuration = $section->getThirdPartySetting('ui_styles', 'regions', []);
         foreach ($regions_configuration as $region_name => $region_styles) {
@@ -49,8 +60,7 @@ class LayoutBuilderTrustedCallbacks implements TrustedCallbackInterface {
           $selected = $region_styles['selected'] ?? [];
           /** @var string $extra */
           $extra = $region_styles['extra'] ?? '';
-          $element['layout_builder'][$index]['layout-builder__section'][$region_name] =
-            $styles_manager->addClasses($element['layout_builder'][$index]['layout-builder__section'][$region_name], $selected, $extra);
+          $layout[$region_name] = $styles_manager->addClasses($layout[$region_name], $selected, $extra);
         }
 
         ++$delta;
